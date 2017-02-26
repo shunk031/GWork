bf761c30b0f5a1abb75e499da8cb30c71fdfaca7
Shunsuke KITADA <septemtrio.ager@gmail.com>
2017-02-26 23:08:47 +0900

    add train and validation script

---
+++ GClassifier/train_cross_validation.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+
+import os
+import argparse
+import pandas as pd
+
+from sklearn.preprocessing import LabelEncoder
+from sklearn.cross_validation import StratifiedKFold
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from ast import literal_eval
+from g_preprocess import PREPROCESSED_DATASET_DIR
+from g_classifier import NaiveBayes
+
+if __name__ == '__main__':
+
+    dataset_choices = []
+    for file in os.listdir(PREPROCESSED_DATASET_DIR):
+        for i, result in enumerate(os.path.splitext(file)):
+            if i % 2 == 0:
+                dataset_choices.append(result)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("dataset", choices=dataset_choices)
+    args = parser.parse_args()
+
+    df = pd.read_csv(os.path.join(PREPROCESSED_DATASET_DIR, args.dataset + ".csv"))
+    df = df.dropna()
+
+    print("[ PROCESS ] Encode label to number.")
+    class_le = LabelEncoder()
+    y = class_le.fit_transform(df['category'].values)
+
+    X = df['article'].values
+
+    print("[ PROCESS ] Start 5-fold Cross validation.")
+    skf = StratifiedKFold(y, 5)
+    accuracy = 0
+    for idx, (train_idx, test_idx) in enumerate(skf):
+        print("[ PROCESS ] Now {} th validation".format(idx + 1))
+        print("[ PROCESS ] Split into train data and test data.")
+        X_train, X_test, y_train, y_test = X[train_idx], X[test_idx], y[train_idx], y[test_idx]
+        print("{:11} all: {}, train: {}, test: {}".format("", len(X), len(X_train), len(X_test)))
+
+        # extract article contents
+        print("[ PROCESS ] Extract train article contents.")
+        X_train_doc = [literal_eval(x) for x in X_train]
+        X_test_doc = [literal_eval(x) for x in X_test]
+
+        print("[ PROCESS ] Train Naive Bayes Classifler.")
+        naive_bayes = NaiveBayes()
+        naive_bayes.train(X_train_doc, y_train)
+        print("{:11} {}".format("", naive_bayes))
+        y_pred = naive_bayes.classify_all(X_test_doc)
+
+        y_true = y_test
+        print("[ PROCESS ] {} th Classification report".format(idx + 1))
+        print(classification_report(y_true, y_pred))
+
+        print("[ PROCESS ] {} th Confusion Matrix".format(idx + 1))
+        print("{}".format(class_le.inverse_transform([x for x in range(8)])))
+        print("{}\n".format(confusion_matrix(y_true, y_pred)))
+
+        accuracy += (y_pred == y[test_idx]).sum() / len(y_pred)
+
+    # output average accuracy
+    print("Average Accuracy: {:.2f} [%]".format((accuracy / 5) * 100))
